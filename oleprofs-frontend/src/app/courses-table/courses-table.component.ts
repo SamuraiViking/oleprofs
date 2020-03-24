b161fd85e8bbc35af3922f687ce2beb3e1023258
SamuraiViking <nelson67@stolaf.edu>
2020-03-24 01:20:55 -0500

    add row turns grey on hover

--- oleprofs-frontend/src/app/courses-table/courses-table.component.ts
+++ oleprofs-frontend/src/app/courses-table/courses-table.component.ts
@@ -12,14 +12,20 @@ export class CoursesTableComponent implements OnInit {
   displayedColumns: string[] = ['name', 'gereqs', 'offerings'];
   @Input() courses: any;
   @Output() setHoveredCourse: EventEmitter<any> = new EventEmitter();
+  @Input() hoveredCourse: Course;
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.courses.filter = filterValue.trim().toLowerCase();
   }
 
-  onMouseEnter(course: Course) {
+  onMouseEnter(event: Event, course: Course) {
+    event.target.style.background = "#eee"
     this.setHoveredCourse.emit(course)
+  }
+
+  onMouseLeave(event: Event) {
+    event.target.style.background = "white";
   }
 
   constructor() { }
