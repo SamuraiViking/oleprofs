715fd3565f8a9a4093c5dfd549a5ba20f076d177
SamuraiViking <nelson67@stolaf.edu>
2020-04-06 18:36:59 -0500

    add query file to find profs with no coresponding RMT scores

---
+++ oleprofs-frontend/src/app/scrapers/query.ts
@@ -0,0 +1,9 @@
+import RMTProfs from '../../static/profsData';
+import valid_profs from '../../static/valid_profs'
+
+// RMTProfs = RateMyProfessor Prof
+
+const RMTProfNames = Object.keys(RMTProfs);
+
+console.log(RMTProfNames);
+
